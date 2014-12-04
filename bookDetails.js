60f7d4cb7188d67816bb51f75fd4044c63d4cb8d
BenyaminYehudi <benyamin.yehudi@gmail.com>
2014-12-04 20:33:37 +0200

    Create bookDetails.js

---
+++ bookDetails.js
@@ -0,0 +1,16 @@
+function getNameParameter(){
+ var search = location.search;
+ search = search.replace("?", "");
+ var paramsArray = search.split("&");
+ for (var i=0;i<paramsArray.length;i++) {
+   var entryArray = paramsArray[i].split("=");
+   if (entryArray[0] === "name") {
+     return entryArray[1];    
+   }
+ }
+ return "";
+} 
+
+var nameParameter = getNameParameter();
+var listOfBooks = JSON.parse(localStorage.getItem("refael"));
+
